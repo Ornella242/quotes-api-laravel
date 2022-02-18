15eabfe3c2ad216dbdda3b8580228e8a14316659
Charaf Alade YESSOUFOU <aladecharaf23@gmail.com>
2022-02-18 11:19:40 +0000

    feat: add Category api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('quotes', QuoteController::class);
+Route::resource('category', CategoryController::class );
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
